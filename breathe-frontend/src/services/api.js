16d5ffb8fd2b24c5d090e9389a190a30a4ead8c1
Devika <devikakanigiri28@gmail.com>
2026-05-26 20:45:15 +0530

    Added requirements file

--- breathe-frontend/src/services/api.js
+++ breathe-frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
 
-  baseURL: "https://breathe-esg-ingestion-platform-2.onrender.com/api/",
-
+  baseURL:
+    "https://breathe-esg-ingestion-platform-2.onrender.com/api/",
 });
 
 export default API;
